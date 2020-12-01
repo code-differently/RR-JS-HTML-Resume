ed92d448c9eb0b0bac32e0e6a39e629335529392
Nick Satinover <nsatinover.aws@gmail.com>
2020-11-30 20:32:30 -0500

    updated contact.js for checkForBlank()

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,23 +1,7 @@
-// function checkForBlank() {
-
-//     let isValid = true;
-//     let thisForm = document.getElementById("contactForm").getElementsByTagName('input');
-    
-//     for(let i = 0; i < thisForm.length; i++) {
-//         let thisInput = thisForm[i];
-//         if(thisInput.type.toLowerCase() == 'text') {
-            
-//             if(!thisInput.value.length > 0){
-//                 isValid = false;
-//                 alert('no value entered for ' + thisInput.id);
-//             }
-//         }
-//     }
-
-//     return isValid;
-// }
-
+// Find blank text fields and display alert box to user
 function checkForBlank() {
+    let alertBeg = "No value entered for: ";
+    let alertMessage = "";
 
     let isValid = true;
     let thisForm = document.getElementById("contactForm").getElementsByTagName('input');
@@ -28,15 +12,30 @@ function checkForBlank() {
             
             if(!thisInput.value.length > 0){
                 isValid = false;
-                let labelName = findLabelForInput(thisInput.id.toString());
-                alert('no value entered for ' + labelName);
+                let labelName = findLabelForInput(thisInput.id);
+                concatInputNames(labelName);                
             }
+
         }
     }
 
+    if(!isValid){
+        alert(alertBeg + alertMessage);
+    }
+
     return isValid;
+
+    //Add missing label names to the alert message
+    function concatInputNames(labelName) {
+        if (alertMessage.length > 0) {
+            alertMessage += ", " + labelName;
+        } else {
+            alertMessage += labelName;
+        }
+    }
 }
 
+//Search by input element id to find corresponding htmlFor label and return text context
 function findLabelForInput(inputID) {
     var idVal = inputID.id;
     labels = document.getElementsByTagName('label');
